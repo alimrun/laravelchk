7cf429ba564b751afefa1e6554b7486d534958e5
md.alimrun <md.alimrun@gmail.com>
2021-06-17 20:16:29 +0600

    updated

--- src/Http/Controllers/LaravelchkController.php
+++ src/Http/Controllers/LaravelchkController.php
@@ -10,7 +10,6 @@ class LaravelchkController extends Controller
 {
     public function domain_verification(Request $request)
     {
-        // set post fields
         $post = [
             'username' => $request['username'],
             'purchase_key' => $request['purchase_key'],
@@ -19,26 +18,84 @@ class LaravelchkController extends Controller
         $ch = curl_init('https://check.6amtech.com/api/v1/domain-check');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-        // execute!
         $response = curl_exec($ch);
-        // close the connection, release resources used
         curl_close($ch);
-        // do anything you want with your response
         try {
             if (base64_decode(json_decode($response, true)['active'])) {
-                DB::table('soft_credentials')->insert([
-                    'key' => 'purchase_key',
-                    'value' => $request['purchase_key']
-                ]);
-                DB::table('soft_credentials')->insert([
-                    'key' => 'username',
-                    'value' => $request['username']
-                ]);
+                if (DB::table('soft_credentials')->where(['key' => 'purchase_key'])->first()) {
+                    DB::table('soft_credentials')->where(['key' => 'purchase_key'])->update([
+                        'value' => $request['purchase_key']
+                    ]);
+                } else {
+                    DB::table('soft_credentials')->insert([
+                        'key' => 'purchase_key',
+                        'value' => $request['purchase_key']
+                    ]);
+                }
+
+                if (DB::table('soft_credentials')->where(['key' => 'username'])->first()) {
+                    DB::table('soft_credentials')->where(['key' => 'username'])->update([
+                        'value' => $request['username']
+                    ]);
+                } else {
+                    DB::table('soft_credentials')->insert([
+                        'key' => 'username',
+                        'value' => $request['username']
+                    ]);
+                }
+
                 return redirect()->route('step3');
             }
+            return redirect('domain-register');
+        } catch (\Exception $exception) {
             return back()->withErrors(['msg', 'Invalid purchase key!']);
-        }catch (\Exception $exception){
-            return back()->withErrors(['msg', 'Invalid purchase key!']);
+        }
+    }
+
+    public function activate_software(Request $request)
+    {
+        $post = [
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'username' => $request['username'],
+            'purchase_key' => $request['purchase_key'],
+            'domain' => preg_replace("#^[^:/.]*[:/]+#i", "", url('/')),
+        ];
+        $ch = curl_init('https://check.6amtech.com/api/v1/domain-register');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        try {
+            if (base64_decode(json_decode($response, true)['active'])) {
+
+                if (DB::table('soft_credentials')->where(['key' => 'purchase_key'])->first()) {
+                    DB::table('soft_credentials')->where(['key' => 'purchase_key'])->update([
+                        'value' => $request['purchase_key']
+                    ]);
+                } else {
+                    DB::table('soft_credentials')->insert([
+                        'key' => 'purchase_key',
+                        'value' => $request['purchase_key']
+                    ]);
+                }
+
+                if (DB::table('soft_credentials')->where(['key' => 'username'])->first()) {
+                    DB::table('soft_credentials')->where(['key' => 'username'])->update([
+                        'value' => $request['username']
+                    ]);
+                } else {
+                    DB::table('soft_credentials')->insert([
+                        'key' => 'username',
+                        'value' => $request['username']
+                    ]);
+                }
+
+                return redirect()->route('step3');
+            }
+            return back()->withErrors(['msg', 'Credential does not match.']);
+        } catch (\Exception $exception) {
+            return back()->withErrors(['msg', 'Credential does not match.']);
         }
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace'=>'Laravelpkg\Laravelchk\Http\Controllers'],function (){
     Route::get('domain-verification','LaravelchkController@domain_verification')->name('domain-verification');
+    Route::post('activate-software','LaravelchkController@activate_software')->name('activate-software');
 });
