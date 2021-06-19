deb60f5edc742cb57e635f0f90b7d60d7327fd6c
md.alimrun <md.alimrun@gmail.com>
2021-06-19 17:32:47 +0600

    mid added

--- src/Http/Controllers/LaravelchkController.php
+++ src/Http/Controllers/LaravelchkController.php
@@ -32,4 +32,25 @@ class LaravelchkController extends Controller
             return back();
         }
     }
+
+    public function actch()
+    {
+        $post = [
+            base64_decode('dXNlcm5hbWU=') => env(base64_decode('QlVZRVJfVVNFUk5BTUU=')),//un
+            base64_decode('cHVyY2hhc2Vfa2V5') => env(base64_decode('UFVSQ0hBU0VfQ09ERQ==')),//pk
+            base64_decode('c29mdHdhcmVfaWQ=') => env(base64_decode(base64_decode('U09GVFdBUkVfSUQ='))),//sid
+            base64_decode('ZG9tYWlu') => preg_replace("#^[^:/.]*[:/]+#i", "", url('/')),
+        ];
+        $ch = curl_init(base64_decode('aHR0cHM6Ly9jaGVjay42YW10ZWNoLmNvbS9hcGkvdjEvYWN0aXZhdGlvbi1jaGVjaw=='));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        try {
+            return base64_decode(json_decode($response, true)['active']);
+        } catch (\Exception $exception) {
+            session()->flash('error', base64_decode('SW52YWxpZCBwdXJjaGFzZSBrZXkh'));//ipk
+            return redirect(base64_decode('aHR0cHM6Ly82YW10ZWNoLmNvbS9zb2Z0d2FyZS1hY3RpdmF0aW9u'));
+        }
+    }
 }
