fd49301932fd236b5a6e413eac3140d763ea143f
md.alimrun <md.alimrun@gmail.com>
2021-06-19 13:48:53 +0600

    encrypted

--- src/Http/Controllers/LaravelchkController.php
+++ src/Http/Controllers/LaravelchkController.php
@@ -8,12 +8,12 @@ use Illuminate\Support\Facades\DB;
 
 class LaravelchkController extends Controller
 {
-    public function domain_verification(Request $request)
+    public function dmvf(Request $request)
     {
         $post = [
-            'username' => $request['username'],
-            'purchase_key' => $request['purchase_key'],
-            'domain' => preg_replace("#^[^:/.]*[:/]+#i", "", url('/')),
+            base64_decode('dXNlcm5hbWU=') => $request[base64_decode('dXNlcm5hbWU=')],//un
+            base64_decode('cHVyY2hhc2Vfa2V5') => $request[base64_decode('cHVyY2hhc2Vfa2V5')],//pk
+            base64_decode('ZG9tYWlu') => preg_replace("#^[^:/.]*[:/]+#i", "", url('/')),
         ];
         $ch = curl_init(base64_decode('aHR0cHM6Ly9jaGVjay42YW10ZWNoLmNvbS9hcGkvdjEvZG9tYWluLWNoZWNr'));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -22,12 +22,12 @@ class LaravelchkController extends Controller
         curl_close($ch);
         try {
             if (base64_decode(json_decode($response, true)['active'])) {
-                session()->put('purchase_code', $request['purchase_key']);
-                return redirect()->route('step3');
+                session()->put(base64_decode('cHVyY2hhc2Vfa2V5'), $request[base64_decode('cHVyY2hhc2Vfa2V5')]);//pk
+                return redirect()->route(base64_decode('c3RlcDM='));//s3
             }
             return redirect(base64_decode('aHR0cHM6Ly82YW10ZWNoLmNvbS9zb2Z0d2FyZS1hY3RpdmF0aW9u'));
         } catch (\Exception $exception) {
-            session()->flash('error', 'Invalid purchase key!');
+            session()->flash('error', base64_decode('SW52YWxpZCBwdXJjaGFzZSBrZXkh'));//ipk
             return back();
         }
     }
--- src/routes/web.php
+++ src/routes/web.php
@@ -5,8 +5,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'Laravelpkg\Laravelchk\Http\Controllers', 'middleware' => ['web']], function () {
-    Route::get('domain-verification', 'LaravelchkController@domain_verification')->name('domain-verification');
-
-    Route::get('activate-software', 'LaravelchkController@activation_index')->name('activate-software');
-    Route::post('activate-software', 'LaravelchkController@activate_software');
+    Route::get('dmvf', 'LaravelchkController@dmvf')->name('dmvf');
 });
