3623d601bded894b7890e3004e28364ea0816207
md.alimrun <md.alimrun@gmail.com>
2021-06-20 13:06:43 +0600

    session added

--- src/Http/Controllers/LaravelchkController.php
+++ src/Http/Controllers/LaravelchkController.php
@@ -11,6 +11,8 @@ class LaravelchkController extends Controller
     public function dmvf(Request $request)
     {
         if (self::is_local()) {
+            session()->put(base64_decode('cHVyY2hhc2Vfa2V5'), $request[base64_decode('cHVyY2hhc2Vfa2V5')]);//pk
+            session()->put(base64_decode('dXNlcm5hbWU='), $request[base64_decode('dXNlcm5hbWU=')]);//un
             return redirect()->route(base64_decode('c3RlcDM='));//s3
         } else {
             $post = [
