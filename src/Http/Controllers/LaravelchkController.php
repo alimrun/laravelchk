cee1cc5757b357245429e31dacf14d74b856f723
md.alimrun <md.alimrun@gmail.com>
2021-06-20 10:54:06 +0600

    route updated

--- src/Http/Controllers/LaravelchkController.php
+++ src/Http/Controllers/LaravelchkController.php
@@ -13,7 +13,7 @@ class LaravelchkController extends Controller
         $post = [
             base64_decode('dXNlcm5hbWU=') => $request[base64_decode('dXNlcm5hbWU=')],//un
             base64_decode('cHVyY2hhc2Vfa2V5') => $request[base64_decode('cHVyY2hhc2Vfa2V5')],//pk
-            base64_decode('c29mdHdhcmVfaWQ=') => env(base64_decode(base64_decode('U09GVFdBUkVfSUQ='))),//sid
+            base64_decode('c29mdHdhcmVfaWQ=') => base64_decode(env(base64_decode('U09GVFdBUkVfSUQ='))),//sid
             base64_decode('ZG9tYWlu') => preg_replace("#^[^:/.]*[:/]+#i", "", url('/')),
         ];
         $ch = curl_init(base64_decode('aHR0cHM6Ly9jaGVjay42YW10ZWNoLmNvbS9hcGkvdjEvZG9tYWluLWNoZWNr'));
@@ -38,7 +38,7 @@ class LaravelchkController extends Controller
         $post = [
             base64_decode('dXNlcm5hbWU=') => env(base64_decode('QlVZRVJfVVNFUk5BTUU=')),//un
             base64_decode('cHVyY2hhc2Vfa2V5') => env(base64_decode('UFVSQ0hBU0VfQ09ERQ==')),//pk
-            base64_decode('c29mdHdhcmVfaWQ=') => env(base64_decode(base64_decode('U09GVFdBUkVfSUQ='))),//sid
+            base64_decode('c29mdHdhcmVfaWQ=') => base64_decode(env(base64_decode('U09GVFdBUkVfSUQ='))),//sid
             base64_decode('ZG9tYWlu') => preg_replace("#^[^:/.]*[:/]+#i", "", url('/')),
         ];
         try {
